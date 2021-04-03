f99253cd9521a47ba51748ebc5a5c771ac578983
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2021-04-03 18:54:25 -0300

    feat: :ok_hand: summary and transactions components

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,18 @@
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+
+import { Container } from "./styles";
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src="" alt=""/>
+        </header>
+        <strong>R$1000</strong>
+      </div>
+    </Container>
+  );
+}
