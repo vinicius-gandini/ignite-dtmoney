6c83804bbd9a48c572b4d163d64d6b0ae82fe266
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2021-04-03 17:32:55 -0300

    feat: :ok_hand: header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Header />
       <GlobalStyle />
     </>
   );
