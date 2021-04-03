93d065ec9dcac0987b2d45f1471f7741d32d2618
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2021-04-03 17:19:43 -0300

    feat: :ok_hand: global styles with styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <GlobalStyle />
+    </>
   );
 }
